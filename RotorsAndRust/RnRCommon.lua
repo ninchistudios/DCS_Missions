cad10c6dbe85a950be8f9e3eb6899d11d932a7bd
james-desktop <ninjachimpstudios@gmail.com>
2021-07-09 20:21:58 +1000

    added jtac, LAV and HMMWV

--- RotorsAndRust/RnRCommon.lua
+++ RotorsAndRust/RnRCommon.lua
@@ -1,7 +1,33 @@
-env.info( '*** RNR COMMON START 02 *** ' )
+env.info( '*** RNR COMMON START *** ' )
 
+env.info( '*** Initialising Recovery Tanker *** ' )
 local TexacoCVN=RECOVERYTANKER:New("KMART", "Texaco Group")
 TexacoCVN:SetTACAN(10, "TXO")
 TexacoCVN:SetRadio(251)
 TexacoCVN:SetCallsign(CALLSIGN.Tanker.Texaco)
 TexacoCVN:Start(1)
+
+env.info( '*** TODO Initialising JFAC *** ' )
+
+ctld.JTAC_LIMIT_RED = 10 -- max number of JTAC Crates for the RED Side
+ctld.JTAC_LIMIT_BLUE = 10 -- max number of JTAC Crates for the BLUE Side
+
+ctld.JTAC_dropEnabled = true -- allow JTAC Crate spawn from F10 menu
+
+ctld.JTAC_maxDistance = 10000 -- How far a JTAC can "see" in meters (with Line of Sight)
+
+ctld.JTAC_smokeOn_RED = true -- enables marking of target with smoke for RED forces
+ctld.JTAC_smokeOn_BLUE = true -- enables marking of target with smoke for BLUE forces
+
+ctld.JTAC_smokeColour_RED = 4 -- RED side smoke colour -- Green = 0 , Red = 1, White = 2, Orange = 3, Blue = 4
+ctld.JTAC_smokeColour_BLUE = 1 -- BLUE side smoke colour -- Green = 0 , Red = 1, White = 2, Orange = 3, Blue = 4
+
+ctld.JTAC_jtacStatusF10 = true -- enables F10 JTAC Status menu
+
+ctld.JTAC_location = true -- shows location of target in JTAC message
+
+ctld.JTAC_lock =  "all" -- "vehicle" OR "troop" OR "all" forces JTAC to only lock vehicles or troops or all ground units
+
+ctld.JTACAutoLase('REAPER1', 1688)
+
+env.info( '*** RNR COMMON END *** ' )
