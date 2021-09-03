5a56aa3aabc59b885afb4fa6ca4cc7ac6e543493
psychomantis0011 <87172721+psychomantis0011@users.noreply.github.com>
2021-09-03 23:17:36 +1000

    lots
    Added BFM arena to the north.
    
    Added red air client jets near the Su33 spawn point (3x Red FA18, 1x SU33, 1xMIG29S)
    
    Cleaned up waypoints.
    Cleaned up radio presets. Radio convention established as Comm 1 Channel 1 (C1Ch1) as per real FA18 ops.
    Added some audio
    
    JTAC 1 and 2 are CTLD.
    JTAC 3 is native (with talk on). On 272 AM
    Added priority lase for JTAC's to lase high risk target first (like SAMs)  
    
    Balanced Red AI flight model to help with dogfights.
    
    Changed Weapon loadout for all Blue jets.
    
    Changed AI F14's starting airborne.
    
    Added KC135MPRS for easier refuels.
    
    Added A10C (both variants at Rota)
    Added Hueys and a FARP at Roto
    Changed weather and start time.
    Updated mission brief.

---
+++ RotorsAndRust/RnRCommon2.lua
@@ -0,0 +1,45 @@
+env.info( '*** RNR COMMON START *** ' )
+
+env.info( '*** RNR Initialising Carrier Script *** ' )
+Wrench.carrierSetup("CV73", 30, {"SHELL"})
+
+env.info( '*** Initialising Recovery Tanker *** ' )
+local TexacoCVN=RECOVERYTANKER:New("CV73", "TEXACO")
+TexacoCVN:SetTACAN(10, "TXO")
+TexacoCVN:SetRadio(251)
+TexacoCVN:SetCallsign(CALLSIGN.Tanker.Texaco)
+TexacoCVN:Start(1)
+
+env.info( '*** RNR Initialising JFAC *** ' )
+
+ctld.JTAC_LIMIT_RED = 10 -- max number of JTAC Crates for the RED Side
+ctld.JTAC_LIMIT_BLUE = 10 -- max number of JTAC Crates for the BLUE Side
+
+ctld.JTAC_dropEnabled = true -- allow JTAC Crate spawn from F10 menu
+
+ctld.JTAC_maxDistance = 10000 -- How far a JTAC can "see" in meters (with Line of Sight)
+
+ctld.JTAC_smokeOn_RED = true -- enables marking of target with smoke for RED forces
+ctld.JTAC_smokeOn_BLUE = true -- enables marking of target with smoke for BLUE forces
+
+ctld.JTAC_smokeColour_RED = 4 -- RED side smoke colour -- Green = 0 , Red = 1, White = 2, Orange = 3, Blue = 4
+ctld.JTAC_smokeColour_BLUE = 1 -- BLUE side smoke colour -- Green = 0 , Red = 1, White = 2, Orange = 3, Blue = 4
+
+ctld.JTAC_jtacStatusF10 = true -- enables F10 JTAC Status menu
+
+ctld.JTAC_location = true -- shows location of target in JTAC message
+
+ctld.JTAC_lock =  "all" -- "vehicle" OR "troop" OR "all" forces JTAC to only lock vehicles or troops or all ground units
+
+ctld.JTACAutoLase('JTAC1', 1681) -- These were the original JTAC options for Phase 1 and 2
+ctld.JTACAutoLase('JTAC2', 1682)
+-- ctld.JTACAutoLase('JTAC3', 1683)
+
+
+-- RADIO PARAMETERS HAVE BEEN SET FOR COMMS THROUGH SRS --
+
+-- ctld.JTACAutoLase('JTAC1', 1681, true, all, 1 {freq = "270.00", mod = "AM", name = "BLUE-REAPER-1-NORTH-270"})
+-- ctld.JTACAutoLase('JTAC2', 1682, true, all, 1 {freq = "271.00", mod = "AM", name = "BLUE-REAPER-2-CENTRE-271"})
+-- ctld.JTACAutoLase('JTAC3', 1683, true, all, 1 {freq = "272.00", mod = "AM", name = "BLUE-REAPER-3-SOUTH-272"})
+
+env.info( '*** RNR COMMON END *** ' )
